0b50798b1096b8ebcd292a762c2fe4fea566dc20
waynewangyuxuan <yw5954@nyu.edu>
2024-10-07 17:29:06 -0400

    create an example index page. The page is on the path of /index.

--- server/endpoints.py
+++ server/endpoints.py
@@ -4,7 +4,7 @@ The endpoint called `endpoints` will return all available endpoints.
 """
 # from http import HTTPStatus
 
-from flask import Flask  # , request
+from flask import Flask, render_template  # , request
 from flask_restx import Resource, Api  # Namespace, fields
 from flask_cors import CORS
 
@@ -16,11 +16,15 @@ api = Api(app)
 
 ENDPOINT_EP = '/endpoints'
 ENDPOINT_RESP = 'Available endpoints'
+
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
+
 TITLE_EP = '/title'
 TITLE_RESP = 'Title'
 TITLE = 'Jobless Computer Science Student Analysis (JCSS)'
+
+INDEX_EP = '/index' #should be set to "/"
 
 
 @api.route(HELLO_EP)
@@ -61,3 +65,9 @@ class JournalTitle(Resource):
         Retrieve the journal title.
         """
         return {TITLE_RESP: TITLE}
+
+
+
+@app.route(INDEX_EP)
+def index():
+    return render_template('index.html')
