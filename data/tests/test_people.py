115f2710175a14244ee37cc127763aedc6c6e8d0
waynewangyuxuan <yw5954@nyu.edu>
2024-10-10 12:47:14 -0400

    add test for create()

--- data/tests/test_people.py
+++ data/tests/test_people.py
@@ -10,3 +10,21 @@ def test_read():
     for _id, person in people.items():
         assert isinstance(_id, str)
         assert ppl.NAME in person
+
+
+
+ADD_EMAIL = 'joe@nyu.edu'
+
+
+def test_create():
+    people = ppl.read()
+    assert ADD_EMAIL not in people
+    ppl.create('Joe Smith', 'NYU', ADD_EMAIL)
+    people = ppl.read()
+    assert ADD_EMAIL in people
+
+
+def test_create_duplicate():
+    with pytest.raises(ValueError):
+        ppl.create('Do not care about name',
+                          'Or affiliation', ppl.TEST_EMAIL)
