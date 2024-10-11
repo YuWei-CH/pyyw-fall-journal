6f85b40045762aa42f0f7c1c8221bd30933fec3c
Yiiii0 <yiqiaozhou@outlook.com>
2024-10-10 20:06:44 -0400

    add test_read() in test_endpoint.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -11,6 +11,8 @@ from unittest.mock import patch
 
 import pytest
 
+from data.people import NAME 
+
 import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
@@ -22,9 +24,19 @@ def test_hello():
     resp_json = resp.get_json()
     assert ep.HELLO_RESP in resp_json
 
+
 def test_title():
     resp = TEST_CLIENT.get(ep.TITLE_EP)
     resp_json = resp.get_json()
     assert ep.TITLE_RESP in resp_json
     assert isinstance(resp_json[ep.TITLE_RESP], str)
     assert len(resp_json[ep.TITLE_RESP]) > 0
+
+
+def test_read():
+    resp = TEST_CLIENT.get(ep.PEOPLE_EP)
+    resp_json = resp.get_json()
+    for _id, person in resp_json.items():
+        assert isinstance(_id, str)
+        assert len(_id) > 0
+        assert NAME in person
