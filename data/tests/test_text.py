28767c24cc4931705c4b14300b21c7e1e17c2313
Yiiii0 <yiqiaozhou@outlook.com>
2024-10-24 17:47:26 -0400

    add tests for delete() in test_text.py

--- data/tests/test_text.py
+++ data/tests/test_text.py
@@ -13,3 +13,17 @@ def test_read_one():
 
 def test_read_one_not_found():
     assert txt.read_one('Not a page key!') == {}
+
+
+def test_delete():
+    texts = txt.read()
+    old_len = len(texts)
+    txt.delete(txt.DEL_KEY)
+    texts = txt.read()
+    assert len(texts) < old_len
+    assert txt.DEL_KEY not in texts
+
+
+def test_delete_not_found():
+    result = txt.delete('Not a page key!')
+    assert result is None
