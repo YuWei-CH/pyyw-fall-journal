49f63b504d24e875119b921a1c480c5ff8e9632c
waynewangyuxuan <yw5954@nyu.edu>
2024-10-26 19:18:44 -0400

    add get_masthead_roles in roles.py

--- data/roles.py
+++ data/roles.py
@@ -5,6 +5,8 @@ This module manages person roles for a journal.
 AUTHOR_CODE = 'AU'
 EDITOR_CODE = 'ED'
 REFREE_CODE = 'RE'
+MNG_EDITOR_CODE = 'ME'
+CP_EDITOR_CODE = 'CE'
 
 TEST_CODE = AUTHOR_CODE
 
@@ -12,7 +14,12 @@ ROLES = {
     AUTHOR_CODE: 'Author',
     EDITOR_CODE: 'Editor',
     REFREE_CODE: 'Referee',
+    MNG_EDITOR_CODE: 'Managing Editor',
+    CP_EDITOR_CODE: 'Copy Editor'
+
 }
+
+MH_ROLES = [EDITOR_CODE, MNG_EDITOR_CODE, CP_EDITOR_CODE]
 
 
 def get_roles() -> dict:
@@ -23,8 +30,20 @@ def is_valid(code: str) -> bool:
     return code in ROLES
 
 
+def get_masthead_roles() -> dict:
+    mh_roles = get_roles()
+    del_mh_roles = []
+    for role in mh_roles:
+        if role not in MH_ROLES:
+            del_mh_roles.append(role)
+    for del_role in del_mh_roles:
+        del mh_roles[del_role]
+    return mh_roles
+
+
 def main():
     print(get_roles())
+    print(get_masthead_roles())
 
 
 if __name__ == '__main__':
