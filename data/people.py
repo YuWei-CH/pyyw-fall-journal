e3cb7c5bcd223d6df74f772523806660e5637b50
Yirong Wang <yw5490@nyu.edu>
2024-11-14 18:21:12 -0500

    update is_valid_person() to have it be able to check if a list of roles is valid

--- data/people.py
+++ data/people.py
@@ -55,13 +55,18 @@ def read():
 
 
 def is_valid_person(name: str, affiliation: str, email: str,
-                    role: str) -> bool:
+                    role: str = None, roles: list = None) -> bool:
     if email in people_dict:
         raise ValueError(f'Adding duplicate {email=}')
     if not is_valid_email(email):
         raise ValueError(f'Invalid email: {email}')
-    if not rls.is_valid(role):
-        raise ValueError(f'Invalid role: {role}')
+    if role:
+        if not rls.is_valid(role):
+            raise ValueError(f'Invalid role: {role}')
+    elif roles:
+        for role in roles:
+            if not rls.is_valid(role):
+                raise ValueError(f'Invalid role: {role}')
     return True
 
 
