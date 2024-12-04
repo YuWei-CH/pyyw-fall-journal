3313f949d067294ea266e61e5a74aaeacbb929fd
waynewangyuxuan <yw5954@nyu.edu>
2024-12-04 11:38:50 -0500

    add tests for query

---
+++ data/tests/test_query.py
@@ -0,0 +1,31 @@
+import random
+import pytest
+import data.manuscripts.query as mqry
+
+
+def gen_random_not_valid_str() -> str:
+    """
+    That huge number is only important in being huge:
+        any big number would do.
+    """
+    BIG_NUM = 10_000_000_000
+    big_int = random.randint(0, BIG_NUM)
+    big_int += BIG_NUM
+    bad_str = str(big_int)
+
+
+def test_is_valid_state():
+    for state in mqry.get_states():
+        assert mqry.is_valid_state(state)
+
+
+def test_is_not_valid_state():
+    # run this test "a few" times
+    for i in range(10):
+        assert not mqry.is_valid_state(gen_random_not_valid_str())
+
+
+def test_is_valid_action():
+    for action in mqry.get_actions():
+        assert mqry.is_valid_action(action)
+
