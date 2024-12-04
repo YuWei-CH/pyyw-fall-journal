ab28b123a80f5cf5497ba7bb6bcf0b61642a1bfc
waynewangyuxuan <yw5954@nyu.edu>
2024-12-04 11:39:59 -0500

    add more tests for query

--- data/tests/test_query.py
+++ data/tests/test_query.py
@@ -29,3 +29,28 @@ def test_is_valid_action():
     for action in mqry.get_actions():
         assert mqry.is_valid_action(action)
 
+
+def test_is_not_valid_action():
+    # run this test "a few" times
+    for i in range(10):
+        assert not mqry.is_valid_action(gen_random_not_valid_str())
+
+
+def test_handle_action_bad_state():
+    with pytest.raises(ValueError):
+        mqry.handle_action(gen_random_not_valid_str(),
+                           mqry.TEST_ACTION)
+
+
+def test_handle_action_bad_action():
+    with pytest.raises(ValueError):
+        mqry.handle_action(mqry.TEST_STATE,
+                           gen_random_not_valid_str())
+
+
+def test_handle_action_valid_return():
+    for state in mqry.get_states():
+        for action in mqry.get_actions():
+            new_state = mqry.handle_action(state, action)
+            assert mqry.is_valid_state(new_state)
+
