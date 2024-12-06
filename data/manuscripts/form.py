235e150a703e1323398339865e43d1c77010f23d
Yirong Wang <yw5490@nyu.edu>
2024-12-05 20:14:58 -0500

    added get_form() and get_form_descr() in form.py

--- data/manuscripts/form.py
+++ data/manuscripts/form.py
@@ -9,10 +9,26 @@ FORM_FLDS = [
         ff.PARAM_TYPE: ff.QUERY_STR,
     },
 ]
+
+
+def get_form() -> list:
+    return FORM_FLDS
+
+
+def get_form_descr():
+    """
+    For Swagger!
+    """
+    return ff.get_form_descr(FORM_FLDS)
+
+
 def get_fld_names() -> list:
     return ff.get_fld_names(FORM_FLDS)
 
+
 def main():
+    print(f'Form: {get_form()=}')
+    print(f'Form: {get_form_descr()=}')
     print(f'Field names: {get_fld_names()=}')
 
 
