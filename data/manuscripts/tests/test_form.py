fe8d4b8c39d2e8d60aea4701c21691edb01d6861
Yirong Wang <yw5490@nyu.edu>
2024-12-05 20:24:03 -0500

    added test for get_form() and get_form_descr() in test_form.py

--- data/manuscripts/tests/test_form.py
+++ data/manuscripts/tests/test_form.py
@@ -1,7 +1,22 @@
-import data.manuscripts.form as mform
-from data.manuscripts.fields import TITLE, TEST_FLD_DISP_NM
+import data.manuscripts.form as fm
+import data.manuscripts.form_filler as ff
+
+def test_get_form():
+    form = fm.get_form()
+    assert isinstance(form, list)
+    assert len(form) > 0
+    for fld in form:
+        # Every field must have a name!
+        assert ff.FLD_NM in fld
+        # And it can't be blank.
+        assert len(fld[ff.FLD_NM]) > 0
+
+
+def test_get_form_descr():
+    form_descr = fm.get_form_descr()
+    assert isinstance(form_descr, dict)
 
 
 def test_get_fld_names():
-    names = mform.get_fld_names()
+    names = fm.get_fld_names()
     assert isinstance(names, list)
